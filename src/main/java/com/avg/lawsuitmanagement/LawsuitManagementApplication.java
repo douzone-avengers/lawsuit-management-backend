277a7f5b6f7b2489f472ea7e0336767119830c2e
ksj2083 <ksj2083@naver.com>
2023-07-11 10:42:13 +0900

    최초 커밋 필수 의존성 추가

---
+++ src/main/java/com/avg/lawsuitmanagement/LawsuitManagementApplication.java
@@ -0,0 +1,13 @@
+package com.avg.lawsuitmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawsuitManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawsuitManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avg/lawsuitmanagement/LawsuitManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avg.lawsuitmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawsuitManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
