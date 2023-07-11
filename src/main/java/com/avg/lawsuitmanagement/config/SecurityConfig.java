91e73033aa3a7649a3854aa7bbf09e0e89cdc1ab
ksj2083 <ksj2083@naver.com>
2023-07-11 13:16:53 +0900

    feat : db 연결 기본 설정 및 security config 기본설정 db는 rds에 연결하며, password등 db정보는 별도 properties에 추가하여 gitignore로 관리한다.

---
+++ src/main/java/com/avg/lawsuitmanagement/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.avg.lawsuitmanagement.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .httpBasic().disable()
+            .csrf().disable() //세션 사용하지 않으므로, csrf 보안 정책 필요x
+            .cors().configurationSource(corsConfigurationSource())
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+//            .antMatchers().permitAll() //열어줄 요청들 표기
+            .anyRequest().authenticated() //나머지 요청은 인증 필요
+
+            .and()
+            .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+}
