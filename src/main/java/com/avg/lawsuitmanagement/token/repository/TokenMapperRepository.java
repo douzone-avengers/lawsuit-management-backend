11f9e0b2bc9faae3574fd3128e5cc11d863b4816
ksj2083 <ksj2083@naver.com>
2023-07-12 16:26:10 +0900

    refactor : 필요없는 메소드 제거

--- src/main/java/com/avg/lawsuitmanagement/token/repository/TokenMapperRepository.java
+++ src/main/java/com/avg/lawsuitmanagement/token/repository/TokenMapperRepository.java
@@ -7,10 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TokenMapperRepository {
-
-    ClientDto selectClientByIdAndPassword(ClientLoginParam param);
     ClientDto selectClientById(String email);
 
     void insertClient(ClientSignUpParam param);
-
 }
