934fe90fe0d79c9c70d3a3610005391e73f4cbb7
ksj2083 <ksj2083@naver.com>
2023-07-17 17:08:46 +0900

    feat : 인증/인가 구현 시큐리티config 설정에 따라 모든요청에 대해서 "인증"과정을 거친다. 적법하게 발행된 Baerer 토큰을 Header에 장착한 요청만 허용된다. 그 후 설정에 따라 "인가" 과정을 거친다. 예시로 작성된 test 메소드의 경우 "ADMIN" 권한을 가지고 있어야 한다. 참고) DB의 MEMBER 테이블에 저장된 값에 따라 다음과 같은 권한을 가진다. CLIENT : ROLE_CLIENT, EMPLOYEE : ROLE_EMPLOYEE, ADMIN: ROLE_EMPLOYEE, ROLE_ADMIN

--- src/main/java/com/avg/lawsuitmanagement/token/provider/TokenProvider.java
+++ src/main/java/com/avg/lawsuitmanagement/token/provider/TokenProvider.java
@@ -2,20 +2,30 @@ package com.avg.lawsuitmanagement.token.provider;
 
 
 import com.avg.lawsuitmanagement.token.dto.JwtTokenDto;
+import com.avg.lawsuitmanagement.token.dto.MemberDto;
+import com.avg.lawsuitmanagement.token.service.CustomUserDetail;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class TokenProvider {
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 10; //10초
+
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 5; //5분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60; //60분
     private final Key key; //토큰 암호화에 사용되는 비밀키
 
@@ -46,10 +56,57 @@ public class TokenProvider {
             .build();
     }
 
+    /*
+    토큰이 적법한지 검증한다.
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+        return false;
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+
+//        String[] authorityArr = claims.get("auth").toString().split(",");
+//        List<GrantedAuthority> authorityList = new ArrayList<>();
+//        for(String auth : authorityArr) {
+//            authorityList.add(new SimpleGrantedAuthority(auth));
+//        }
+
+        List<GrantedAuthority> authorityList =
+            Arrays.stream(claims.get("auth").toString().split(","))
+                .map((String authority) -> new SimpleGrantedAuthority(authority))
+                .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+
+        MemberDto memberDto = MemberDto.builder()
+            .name(claims.getSubject())
+            .build();
+
+        CustomUserDetail customUserDetail = new CustomUserDetail(memberDto);
+
+        return new UsernamePasswordAuthenticationToken(customUserDetail, "", authorityList);
+    }
+
+
     //AccessToken 발행
     private String createAccessToken(long now, Authentication authentication) {
 
-        //해당 유저의 권한 목록 ex) ROLE_ADMIN, ROLE_CUSTOMER
+        //해당 유저의 권한 목록 ex) ROLE_ADMIN, ROLE_EMPLOYEE
+
         String authorities = authentication.getAuthorities().stream()
             .map((GrantedAuthority grantedAuthority) -> grantedAuthority.getAuthority())
             .collect(Collectors.joining(","));
@@ -68,5 +125,10 @@ public class TokenProvider {
             .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
+    }
+
+    private Claims parseClaims(String accessToken) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken)
+            .getBody();
     }
 }
