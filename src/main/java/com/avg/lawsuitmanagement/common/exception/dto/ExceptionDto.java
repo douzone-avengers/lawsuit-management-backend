e4a66ea7574d1e53b954cfcfb1f5575220e28325
ksj2083 <ksj2083@naver.com>
2023-07-24 15:06:06 +0900

    fix : @Buileder 사용했는데, @AllArgsConstructor 빠져있던 버그 수정

--- src/main/java/com/avg/lawsuitmanagement/common/exception/dto/ExceptionDto.java
+++ src/main/java/com/avg/lawsuitmanagement/common/exception/dto/ExceptionDto.java
@@ -1,10 +1,12 @@
 package com.avg.lawsuitmanagement.common.exception.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 @Builder
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class ExceptionDto {
     private String code;
     private String message;
