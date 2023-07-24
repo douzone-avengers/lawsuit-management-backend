956cb82271c4265a7b76a8a9b8424c91e627e967
ksj2083 <ksj2083@naver.com>
2023-07-24 15:25:50 +0900

    fix : ExceptionDto에 @Getter 빠진 버그 수정 Getter 빼먹으면 Handler에서 toJson을 못해서 HttpMediaTypeNotAcceptableException 발생한다.

--- src/main/java/com/avg/lawsuitmanagement/common/exception/dto/ExceptionDto.java
+++ src/main/java/com/avg/lawsuitmanagement/common/exception/dto/ExceptionDto.java
@@ -2,11 +2,13 @@ package com.avg.lawsuitmanagement.common.exception.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Builder
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Getter
 public class ExceptionDto {
     private String code;
     private String message;
