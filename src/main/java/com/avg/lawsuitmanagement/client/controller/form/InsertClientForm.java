b89e466395faff6df6d145cef502f1a228406b02
한상진 <tkdwls6044@gmail.com>
2023-08-02 19:00:29 +0900

    feat: 의뢰인 등록 서비스 구현

--- src/main/java/com/avg/lawsuitmanagement/client/controller/form/InsertClientForm.java
+++ src/main/java/com/avg/lawsuitmanagement/client/controller/form/InsertClientForm.java
@@ -1,7 +1,6 @@
 package com.avg.lawsuitmanagement.client.controller.form;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.Getter;
