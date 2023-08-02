c15202f93cc29903e2214baaed02b2660a499877
한상진 <tkdwls6044@gmail.com>
2023-08-02 16:59:53 +0900

    feat: 의뢰인 등록 서비스 구현

--- src/main/java/com/avg/lawsuitmanagement/client/service/ClientService.java
+++ src/main/java/com/avg/lawsuitmanagement/client/service/ClientService.java
@@ -1,9 +1,12 @@
 package com.avg.lawsuitmanagement.client.service;
 
+import static com.avg.lawsuitmanagement.common.exception.type.ErrorCode.CLIENT_ALREADY_EXIST;
 import static com.avg.lawsuitmanagement.common.exception.type.ErrorCode.CLIENT_NOT_FOUND;
 
+import com.avg.lawsuitmanagement.client.controller.form.InsertClientForm;
 import com.avg.lawsuitmanagement.client.dto.ClientDto;
 import com.avg.lawsuitmanagement.client.repository.ClientMapperRepository;
+import com.avg.lawsuitmanagement.client.repository.param.InsertClientParam;
 import com.avg.lawsuitmanagement.common.custom.CustomRuntimeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,5 +27,17 @@ public class ClientService {
         }
 
         return clientDto;
+    }
+
+    public void insertClient(InsertClientForm form) {
+        ClientDto clientDto = clientMapperRepository.selectClientByEmail(form.getEmail());
+
+        // 이미 등록된 고객이면
+        if (clientDto != null) {
+            throw new CustomRuntimeException(CLIENT_ALREADY_EXIST);
+        }
+
+        // 등록된 고객이 아니면 db 입력
+        clientMapperRepository.insertClient(InsertClientParam.of(form));
     }
 }
