e24caf4daab6a1bfe658c31aee065e25ab72d2d6
한상진 <tkdwls6044@gmail.com>
2023-08-06 20:22:52 +0900

    fix: PagingDto 어노테이션 오류 수정

--- src/main/java/com/avg/lawsuitmanagement/common/util/dto/PagingDto.java
+++ src/main/java/com/avg/lawsuitmanagement/common/util/dto/PagingDto.java
@@ -1,14 +1,16 @@
 package com.avg.lawsuitmanagement.common.util.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class PagingDto {
     private int offset; // 시작점
     private int limit;  // 한 페이지에 나타낼 개수
