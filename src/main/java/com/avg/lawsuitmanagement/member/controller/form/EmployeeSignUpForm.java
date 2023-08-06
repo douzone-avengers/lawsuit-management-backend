8a65273facd6f44d719b1ffbef9e1234e8d892cf
ksj2083 <ksj2083@naver.com>
2023-08-07 00:28:26 +0900

    fix : long type에는 notBlank를 사용할 수 없다. NotNull로 수정하였음

--- src/main/java/com/avg/lawsuitmanagement/member/controller/form/EmployeeSignUpForm.java
+++ src/main/java/com/avg/lawsuitmanagement/member/controller/form/EmployeeSignUpForm.java
@@ -2,6 +2,7 @@ package com.avg.lawsuitmanagement.member.controller.form;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,8 +24,8 @@ public class EmployeeSignUpForm {
     private String phone;
     @NotBlank
     private String address;
-    @NotBlank
+    @NotNull
     private long hierarchyId;
-    @NotBlank
+    @NotNull
     private long roleId;
 }
