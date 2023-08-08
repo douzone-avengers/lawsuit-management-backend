b4b5dfb4e249eb7bfee2acd1f0e158ecaf2a69d3
ksj2083 <ksj2083@naver.com>
2023-08-08 16:32:27 +0900

    feat: 개인정보 수정 구현

--- src/main/java/com/avg/lawsuitmanagement/member/repository/MemberMapperRepository.java
+++ src/main/java/com/avg/lawsuitmanagement/member/repository/MemberMapperRepository.java
@@ -2,6 +2,7 @@ package com.avg.lawsuitmanagement.member.repository;
 
 import com.avg.lawsuitmanagement.member.dto.MemberDto;
 import com.avg.lawsuitmanagement.member.repository.param.InsertMemberParam;
+import com.avg.lawsuitmanagement.member.repository.param.UpdateMemberParam;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -11,4 +12,5 @@ public interface MemberMapperRepository {
     MemberDto selectMemberByEmail(String email);
     void insertMember(InsertMemberParam param);
     List<MemberDto> selectMemberListById(List<Long> memberExistList);
+    void updateMember(UpdateMemberParam param);
 }
---
+++ src/main/java/com/avg/lawsuitmanagement/member/repository/param/UpdateMemberParam.java
@@ -0,0 +1,32 @@
+package com.avg.lawsuitmanagement.member.repository.param;
+
+import com.avg.lawsuitmanagement.member.controller.form.PrivateUpdateForm;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UpdateMemberParam {
+
+    private long id;
+    private String email;
+    private String name;
+    private String phone;
+    private String address;
+    private long hierarchyId;
+    private long roleId;
+
+    public static UpdateMemberParam of(PrivateUpdateForm form, long id) {
+        return UpdateMemberParam.builder()
+            .id(id)
+            .email(form.getEmail())
+            .name(form.getName())
+            .phone(form.getPhone())
+            .address(form.getAddress())
+            .hierarchyId(form.getHierarchyId())
+            .roleId(form.getRoleId())
+            .build();
+    }
+}
