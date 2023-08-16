9424b3dde7f76fa845ad19184f033a290f6c6b7c
박지혁 <lhseqrhs@gmail.com>
2023-08-16 14:51:59 +0900

    feat: Add Lawsuit Persistence Layer
    - 사건 공통 헤더에서 필요한 데이터를 위함

--- src/main/java/com/avg/lawsuitmanagement/lawsuit/repository/LawsuitMapperRepository.java
+++ src/main/java/com/avg/lawsuitmanagement/lawsuit/repository/LawsuitMapperRepository.java
@@ -2,6 +2,7 @@ package com.avg.lawsuitmanagement.lawsuit.repository;
 
 import com.avg.lawsuitmanagement.client.dto.ClientLawsuitCountDto;
 import com.avg.lawsuitmanagement.client.repository.param.SelectClientLawsuitListParam;
+import com.avg.lawsuitmanagement.lawsuit.dto.LawsuitBasicRawDto;
 import com.avg.lawsuitmanagement.lawsuit.dto.LawsuitDto;
 import com.avg.lawsuitmanagement.lawsuit.repository.param.InsertLawsuitClientMemberIdParam;
 import com.avg.lawsuitmanagement.lawsuit.repository.param.InsertLawsuitParam;
@@ -11,20 +12,37 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LawsuitMapperRepository {
+
     List<LawsuitDto> selectClientLawsuitList(SelectClientLawsuitListParam param);
+
     LawsuitDto selectLawsuitById(long lawsuitId);
+
     void insertLawsuit(InsertLawsuitParam param);
+
     Long getLastInsertedLawsuitId();
+
     void insertLawsuitClientMap(InsertLawsuitClientMemberIdParam param);
+
     void insertLawsuitMemberMap(InsertLawsuitClientMemberIdParam param);
+
     List<LawsuitDto> selectLawsuitList();
+
     void updateLawsuitInfo(UpdateLawsuitInfoParam param);
+
     List<Long> selectMemberByLawsuitId(long lawsuitId);
+
     void deleteLawsuitInfo(long lawsuitId);
+
     void deleteLawsuitClientMap(long lawsuitId);
+
     void deleteLawsuitMemberMap(long lawsuitId);
+
     void deleteLawsuitClientMapByClientId(long clientId);
+
     List<ClientLawsuitCountDto> selectLawsuitCountByClientId(long clientId);
+
     List<LawsuitDto> selectLawsuitByClientId(long clientId);
+
+    List<LawsuitBasicRawDto> selectBasicLawInfo(Long lawsuitId);
 
 }
