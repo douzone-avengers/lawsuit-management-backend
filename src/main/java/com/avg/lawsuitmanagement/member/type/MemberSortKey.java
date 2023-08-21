1363371032d349af39d29d1f8b9a997ecd1f6e2d
ksj2083 <ksj2083@naver.com>
2023-08-21 14:39:43 +0900

    feat : 사원 검색조건에 추가 조건 추가

--- src/main/java/com/avg/lawsuitmanagement/member/type/MemberSortKey.java
+++ src/main/java/com/avg/lawsuitmanagement/member/type/MemberSortKey.java
@@ -10,7 +10,9 @@ public enum MemberSortKey {
     CREATED_AT("가입일", "created_at"),
     HIERARCHY("직급", "hierarchy_id"),
     ROLE("권한", "role_id"),
-    EMAIL("이메일", "email");
+    EMAIL("이메일", "email"),
+    NAME("이름", "name"),
+    PHONE("전화번호", "phone");
 
     private final String nameKr;
     private final String queryString;
