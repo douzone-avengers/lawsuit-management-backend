cec58f2656be91e479fbaff1d1cb5d9acb3fe956
한상진 <tkdwls6044@gmail.com>
2023-09-13 15:31:03 +0900

    fix: '사건-지출' 지출 증빙자료 파일 다운로드 기능 구현

--- src/main/java/com/avg/lawsuitmanagement/file/controller/FileController.java
+++ src/main/java/com/avg/lawsuitmanagement/file/controller/FileController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @RestController
@@ -22,7 +21,7 @@ public class FileController {
 
     // 파일 다운로드
     @GetMapping("/download/{fileId}")
-    public ResponseEntity<?> download(@PathVariable Long fileId) throws IOException {
+    public ResponseEntity<?> download(@PathVariable Long fileId) {
         byte[] fileData = fileService.getFile(fileId);
         FileMetaDto fileMetaDto = fileService.selectFileById(fileId);
         // MIME 타입을 기본값으로 설정 (예: "application/octet-stream")
